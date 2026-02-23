7645eefd688bc931b3cd43e551c8b2652bcbc5d6
gitazingarana <gitazingarana49@gmail.com>
2026-02-23 18:51:28 +0300

    first iteration

--- src/components/searching.js
+++ src/components/searching.js
@@ -3,9 +3,19 @@ import {rules, createComparison} from "../lib/compare.js";
 
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
+     const ruleNames = [];
+     const searchRules = [ 
+        rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'])
+    ];
+
+    // Передаем настройки вторым аргументом, как и планировалось
+    const compare = createComparison(ruleNames, searchRules);
 
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
-        return data;
+        const searchValue = state[searchField];
+        if (searchValue) return data;
+        
+        return data.filter(row => compare(row, state));
     }
 }
